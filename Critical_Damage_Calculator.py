11f1527e35f13e9e947eaf97c52d610660109309
CrossDarkrix <crosseddarkrix@gmail.com>
2021-07-18 14:45:59 +0900

    Update Critical_Damage_Calculator.py

--- Critical_Damage_Calculator.py
+++ Critical_Damage_Calculator.py
@@ -1,21 +1,33 @@
-﻿from math import floor as flr
-
+#!/usr/bin/env python3
+from math import floor as flr
+import sys
 print('クリティカルダメージ計算ツール\n')
 
 try:
-	BasicSTR = 150 + (int(input('STR値(ステータス): ')) / 5)
-except:
-	BasicSTR = 150 + (255 / 5)
-	
+	STR = int(input('STR値(ステータス): '))
+	StrPercent = STR * (int(input('STR値(%)の合計値: ')) / 100)
+	AllSTR = 150 + ((StrPercent + STR) / 5)
+	CRtSTR = 150 + (STR / 5)
+except ValueError:
+	AllSTR = 150 + (255 / 5)
+	CRtSTR = 150 + (255 / 5)
+except KeyboardInterrupt:
+	print()
+	sys.exit(0)
 try:
 	CriticalDamage = int(input('合計クリティカルダメージ: '))
-except:
+except ValueError:
 	CriticalDamage = 1
+except KeyboardInterrupt:
+	print()
+	sys.exit(0)
 try:
-	CriticalDamage_Percent = BasicSTR * (int(input('合計クリティカルダメージ(%): ')) / 100)
-except:
+	CriticalDamage_Percent = CRtSTR * (int(input('合計クリティカルダメージ(%): ')) / 100)
+except ValueError:
 	CriticalDamage_Percent = 1
+except KeyboardInterrupt:
+	print()
+	sys.exit(0)
 
-AllCriticalDamage = BasicSTR + CriticalDamage + flr(CriticalDamage_Percent)
+AllCriticalDamage = AllSTR + CriticalDamage + flr(CriticalDamage_Percent)
 print("\nあなたのクリティカルダメージは: {CRTDAMAGE}です".format(CRTDAMAGE=flr(AllCriticalDamage)))
-input()
